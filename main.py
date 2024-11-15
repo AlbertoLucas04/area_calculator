61ff403bd618ba89fd8d92c7618d91a397bc0545
AlbertoLucas04 <abertolucas12@gmail.com>
2024-11-15 18:42:14 -0300

    adição de referências nas áreas

--- main.py
+++ main.py
@@ -24,17 +24,17 @@ match options:
         base_q = float(input('Insira o valor da Base: '))
         altura_q = float(input('Insira o valor da Altura: '))
         result_q = area_quadrado_retangulo(base_q, altura_q)
-        print(f'\nA = B x H\nA = {base_q} x {altura_q}\nA = {result_q} u.')
+        print(f'\nA(Quadrado) = B x H\nA = {base_q} x {altura_q}\nA = {result_q} u.')
     case 2:
         base_t = float(input('Insira o valor da Base: '))
         altura_t = float(input('Insira o valor da Altura: '))
         result_t = area_triangulo(base_t, altura_t)
-        print(f'\nA = (B x H) / 2 \nA = ({base_t} x {altura_t}) / 2\nA = {result_t} u.')
+        print(f'\nA(Triângulo) = (B x H) / 2 \nA = ({base_t} x {altura_t}) / 2\nA = {result_t} u.')
     case 3:
         raio_c = float(input('Insira o Raio: '))
         result_c = area_circulo(raio_c)
-        print(f'\nA = π x Rˆ2 \nA = π x {raio_c}ˆ2 \nA = {ceil(result_c)} u.')
-
+        print(f'\nA(Círculo) = π x Rˆ2 \nA = π x {raio_c}ˆ2 \nA ~= {ceil(result_c)} u.')
+        
 
 
 
